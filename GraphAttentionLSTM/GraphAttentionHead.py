4c13495210716e04963f1ef641a665b547b0db48
NuoWenLei <nuowen0612@gmail.com>
2022-06-26 22:13:20 +0800

    add graph attention LSTM modules

---
+++ GraphAttentionLSTM/GraphAttentionHead.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+
+class GraphAttentionHead(tf.keras.layers.Layer):
+
+  def __init__(self, sequence_length: int, output_size: int, activation, residual: bool, use_bias: bool, name: str):
+    super().__init__(name = name)
+    self.seq_len = sequence_length
+    self.output_size = output_size
+    self.residual = residual
+    self.use_bias = use_bias
+    self.activation = activation
+    self.bias_initializer = tf.keras.initializers.RandomUniform(minval=-0.5, maxval=0.5, seed=None)
+
+    self.conv_sequence_feature_generation = tf.keras.layers.Conv1D(self.output_size, 1)
+    self.conv_self_attention_dim_1 = tf.keras.layers.Conv1D(1, 1)
+    self.conv_self_attention_dim_2 = tf.keras.layers.Conv1D(1, 1)
+    self.leaky_relu = tf.keras.layers.LeakyReLU()
+    self.bias_value = tf.Variable(self.bias_initializer(shape = (self.seq_len, 1)))
+
+    if self.seq_len != self.output_size:
+      self.conv_residual = tf.keras.layers.Conv1D(self.output_size, 1)
+
+  def call(self, inputs):
+    if self.use_bias:
+      sequence = inputs[0]
+      bias_mat = inputs[1]
+    else:
+      sequence = inputs
+
+    seq_feature_tokens = self.conv_sequence_feature_generation(sequence) # output shape: (seq_len, output_size)
+
+    seq_att_dim_1 = self.conv_self_attention_dim_1(seq_feature_tokens) # output shape: (seq_len, 1)
+    seq_att_dim_2 = self.conv_self_attention_dim_2(seq_feature_tokens) # output shape: (seq_len, 1)
+
+    non_softmax_attention = seq_att_dim_1 + tf.transpose(seq_att_dim_2, [0, 2, 1]) # output shape: (seq_len, seq_len)
+    if self.use_bias:
+      non_softmax_attention = non_softmax_attention + bias_mat # output shape: (seq_len, seq_len)
+    softmax_attention_score = tf.keras.activations.softmax(non_softmax_attention) # output shape: (seq_len, seq_len)
+
+    attention_applied_sequence_features = tf.matmul(softmax_attention_score, seq_feature_tokens) # output shape: (seq_len, output_size)
+    attention_applied_sequence_features_w_bias = attention_applied_sequence_features + self.bias_value # output shape: (seq_len, output_size)
+
+    if self.residual:
+      if self.seq_len != self.output_size:
+        attention_applied_sequence_features_w_bias = attention_applied_sequence_features_w_bias + self.conv_residual(sequence)
+      else:
+        attention_applied_sequence_features_w_bias = attention_applied_sequence_features_w_bias + sequence
+    
+    if self.activation:
+      return self.activation(attention_applied_sequence_features_w_bias)
+
+    return attention_applied_sequence_features_w_bias
+
---
+++ GraphAttentionLSTM/model_utils.py
@@ -0,0 +1,48 @@
+from typing import Iterable
+from MultiHeadGraphAttentionLSTMCell import MultiHeadGraphAttentionLSTMCell, tf
+
+def create_graph_attention_lstm_model(layer_units: Iterable,
+num_heads: int,
+sequence_length: int,
+output_size: int,
+residual: bool,
+use_bias: bool,
+concat_output: bool = False,
+name: str = "GraphAttentionLSTMModel"):
+
+	input_nodes = tf.keras.layers.Input(shape = (7, 49, 5))
+	input_adj_mats = tf.keras.layers.Input(shape = (7, 49, 49))
+
+	x = input_nodes
+
+	for i in range(len(layer_units)):
+		mhgaLSTM_cell = MultiHeadGraphAttentionLSTMCell(
+			units = layer_units[i],
+			num_heads = num_heads,
+			sequence_length = sequence_length,
+			output_size = output_size,
+			residual = residual,
+			concat_output = concat_output,
+			use_bias = use_bias,
+			name = f"{name}_cell_{i}"
+		)
+		x = tf.keras.layers.RNN(
+			mhgaLSTM_cell,
+			return_sequences = True
+		)((x, input_adj_mats))
+
+	mhgaLSTM_out_cell = MultiHeadGraphAttentionLSTMCell(
+		units = layer_units[-1],
+		num_heads = num_heads,
+		sequence_length = sequence_length,
+		output_size = 1,
+		residual = True,
+		concat_output = False,
+		use_bias = True,
+		name = f"{name}_cell_out"
+	)
+
+	mhgaLSTM_2 = tf.keras.layers.RNN(mhgaLSTM_out_cell)((x, input_adj_mats))
+
+	return tf.keras.models.Model(inputs = [input_nodes, input_adj_mats], outputs = mhgaLSTM_2)
+
