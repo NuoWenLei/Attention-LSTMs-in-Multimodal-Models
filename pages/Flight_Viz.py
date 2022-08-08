824f6572f5d46cbc88555a0744acd914e48442bc
NuoWenLei <nuowen0612@gmail.com>
2022-08-09 03:01:46 +0800

    Update Flight_Viz.py

--- pages/Flight_Viz.py
+++ pages/Flight_Viz.py
@@ -27,7 +27,7 @@ if inp_date is not None:
 	edge_weights = [i['count_logged'] for i in dict(G.edges).values()]
 	labels = {i:i for i in dict(G.nodes).keys()}
 	fig, ax = plt.subplots(figsize=(12,5))
-	pos = nx.spring_layout(G)
+	pos = nx.spring_layout(G, seed = 100)
 	nx.draw_networkx_nodes(G, pos, ax = ax)
 	nx.draw_networkx_edges(G, pos, width=edge_weights, ax=ax)
 	_ = nx.draw_networkx_labels(G, pos, labels, ax=ax)
