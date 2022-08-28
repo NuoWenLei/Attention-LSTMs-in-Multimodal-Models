53491c879bae93d5712db3aa4d943fe79641127a
NuoWenLei <nuowen0612@gmail.com>
2022-08-28 11:18:33 -0400

    add model that supports keras tuner

--- imports.py
+++ imports.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 import pandas as pd, networkx as nx, matplotlib.pyplot as plt, numpy as np, json
+import keras_tuner as kt
 from tensorflow.python.training.tracking.data_structures import NoDependency
 from typing import Iterable
 from datetime import date
